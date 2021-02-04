a00a8996323ee3debdc4c0efd663e2c0d9ed357e
Adolfo Sandoval <adolfosandoval@Adolfos-MacBook-Pro.local>
2021-02-04 14:54:02 -0500

    environment added

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -1,15 +1,14 @@
-let APIRUL = "";
+let APIRUL = ''
 
 switch (window.location.hostname) {
   //localhost of react app
-  case "localhost" || "127.0.0.1":
+  case "localhost": 
+  case "127.0.0.1":
     //localhost name of api
-    APIURL = "http://localhost:3000";
-    break;
-  //deployed react app
-  case "mycomfy.herokuapp.com": //client app
-    //server app
-    APIRUL = "https://fast-taiga-38803.herokuapp.com/";
+    APIURL = 'http://localhost:3000'
+    break
+  case 'mycomfy.herokuapp.com': 
+    APIURL = "https://fast-taiga-38803.herokuapp.com/";
 }
 
-export default APIRUL;
+export default APIURL;
